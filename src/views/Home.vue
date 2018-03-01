1a4ffd3628bbebdb839b157eb4c0c464ca21cf13
cjh <1418503647@qq.com>
2018-03-01 21:41:43 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <my-page title="带壳截图">
+        <canvas id="canvas"></canvas>
+        <img class="img" src="/static/img/mi.png">
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        mounted() {
+            this.init()
+        },
+        methods: {
+            init() {
+                let canvas = document.getElementById('canvas')
+                let ctx = canvas.getContext('2d')
+                canvas.style.width = '100px'
+                canvas.style.height = '100px'
+                ctx.width = 100
+                ctx.height = 100
+                ctx.fillRect(0, 0, 20, 20)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.img {
+    width: 300px;
+}
+canvas {
+    background-color: #f00;
+}
+</style>
